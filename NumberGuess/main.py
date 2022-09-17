22184fcdee43b2a8c7b58b55d63fd539e4c5ef32
Emmanuel <official.emmanuel001@gmail.com>
2022-09-17 19:29:52 +0300

    Number guess, added vs computer

--- NumberGuess/main.py
+++ NumberGuess/main.py
@@ -14,4 +14,60 @@ def play(highest):
     
     return 'Congratulations!!! You win!'
 
-print(play(20))
+
+def autoGuess(randValue):
+
+    compValue = random.randint(1,(randValue*randValue)) #higher limit set to square of the correct value
+    count = 1
+
+    while randValue != compValue:
+
+        count = count + 1
+        #if guess too high
+        #reduce upper limit maintain lower limit
+        if compValue > randValue:   #Reduce guessing range
+            newRandom = compValue - 1
+            compValue = random.randint(1, newRandom)
+            print(compValue)
+            
+        #if guess too low   
+        #increase lower limit and maintain higher limit
+        if compValue < randValue:
+            newRandom = compValue + 1
+            compValue = random.randint(newRandom, randValue)   
+            print(compValue)
+
+    print(f"Guessed {count} times")
+    return "Guessed"
+
+
+#involves the user with the system guessing
+def interactiveGuess():
+
+    randValue = int(input("Enter number to be guessed\t"))
+
+    compValue = random.randint(1,(randValue*randValue)) #higher limit set to square of the correct value
+    count = 1
+
+    while randValue != compValue:
+        print(compValue)
+
+        isCorrect = input(" 'h' for guess too high\n 'l' for guess too low\t").lower()
+        
+        #if guess too high
+        if isCorrect == 'h':  
+            newRandom = compValue - 1
+            compValue = random.randint(1, newRandom)
+            
+        #if guess too low   
+        if isCorrect == 'l':
+            newRandom = compValue + 1
+            compValue = random.randint(newRandom, randValue)   
+        
+        count = count + 1
+
+    print(compValue)
+    print(f"Guessed {count} times")
+    return("Yay!!!Computer guessed the number")
+
+print(interactiveGuess())
